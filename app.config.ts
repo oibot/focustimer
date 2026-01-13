e37bbc635ef616eac2c49fa80d7146d4b2a07585
Tobias Ostner <tobias.ostner@gmail.com>
2026-01-13 14:17:49 +0100

    refactor: switch to app config

---
+++ app.config.ts
@@ -0,0 +1,51 @@
+import type { ExpoConfig } from "expo/config"
+
+const variant = process.env.APP_VARIANT
+const name =
+  variant === "dev"
+    ? "pomodoro-dev"
+    : variant === "test"
+      ? "pomodoro-test"
+      : "pomodoro"
+
+const bundleBase = "com.tobio.pomodoro"
+const bundleIdentifier =
+  variant === "dev"
+    ? `${bundleBase}.dev`
+    : variant === "test"
+      ? `${bundleBase}.test`
+      : bundleBase
+
+const config: ExpoConfig = {
+  name,
+  slug: "pomodoro",
+  scheme: "pomodoro",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/icon.png",
+  newArchEnabled: true,
+  splash: {
+    image: "./assets/splash-icon.png",
+    resizeMode: "contain",
+    backgroundColor: "#ffffff",
+  },
+  ios: {
+    supportsTablet: true,
+    bundleIdentifier,
+  },
+  android: {
+    package: bundleIdentifier,
+    adaptiveIcon: {
+      foregroundImage: "./assets/adaptive-icon.png",
+      backgroundColor: "#ffffff",
+    },
+    edgeToEdgeEnabled: true,
+    predictiveBackGestureEnabled: false,
+  },
+  web: {
+    favicon: "./assets/favicon.png",
+  },
+  plugins: ["expo-router"],
+}
+
+export default config
