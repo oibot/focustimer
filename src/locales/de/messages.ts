438b944a956767aa1622a8f0d7ccb64b000ed6eb
Tobias Ostner <tobias.ostner@gmail.com>
2026-02-12 14:29:38 +0100

    Translate the English strings

--- src/locales/de/messages.ts
+++ src/locales/de/messages.ts
@@ -1,1 +1,4 @@
-/*eslint-disable*/import type{Messages}from"@lingui/core";export const messages=JSON.parse("{\"CTofpw\":[\"Keep going\"],\"Fsd1Wl\":[\"Focus\"],\"I82p17\":[\"Start Break\"],\"ICVsT0\":[\"Your current focus timer will reset.\"],\"IgrLD/\":[\"Pause\"],\"Q70Hn2\":[\"Start Focus\"],\"d7Kp2g\":[\"Break\"],\"dEgA5A\":[\"Cancel\"],\"m5Kgfo\":[\"Cancel focus session?\"],\"tXkhj/\":[\"Start\"],\"v39wLo\":[\"Resume\"],\"ygCKqB\":[\"Stop\"]}")as Messages;
+/*eslint-disable*/ import type { Messages } from "@lingui/core"
+export const messages = JSON.parse(
+  '{"CTofpw":["Weitermachen"],"Fsd1Wl":["Fokus"],"I82p17":["Pause starten"],"ICVsT0":["Der aktuelle Fokus-Timer wird zurückgesetzt."],"IgrLD/":["Pausieren"],"Q70Hn2":["Fokus starten"],"d7Kp2g":["Pause"],"dEgA5A":["Abbrechen"],"m5Kgfo":["Fokussitzung abbrechen?"],"tXkhj/":["Start"],"v39wLo":["Fortsetzen"],"ygCKqB":["Stoppen"]}',
+) as Messages
